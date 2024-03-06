3c0b49df0df61797f155079b0d46e193e8fc039f
Meelad0708 <mazizollah@soldevelo.com>
2024-03-06 11:14:04 +0100

    reworked models

--- MM/admin.py
+++ MM/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import MovingMotivator, Team, Member, MemberMotivator
 # Register your models here.
+
+admin.site.register(MovingMotivator)
+admin.site.register(Team)
+admin.site.register(Member)
+admin.site.register(MemberMotivator)
--- MM/models.py
+++ MM/models.py
@@ -2,19 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Moving_Motivators(models.Model):
-    name = models.CharField(max_length=30)
-    date = models.DateField(auto_now_add=True)
-    description = models.CharField(max_length=1000)
-
 
 class Team(models.Model):
     team_name = models.CharField(max_length=50)
-    member = models.ManyToManyField(User, through='Member')
+    team_leader = models.CharField(max_length=50)
+    job_description = models.CharField(max_length=1000)
+    # add logo
+
 
 class Member(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    moving_motivators = models.ManyToManyField(Moving_Motivators)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+
+
+class MovingMotivator(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=1000)
+    # add picture
+
+
+class MemberMotivator(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='motivators')
+    moving_motivator = models.ForeignKey(MovingMotivator, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    # ensure that there are no duplicate of moving motivator for a member
+    # and that motivators appear in order that members have defined
+    class Meta:
+        unique_together = ('member', 'moving_motivator')
+        ordering = ['order']
+
 
 
