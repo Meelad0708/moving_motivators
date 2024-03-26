c7aa1ed6a7df428cc010530f5fac485af5a33f51
Meelad0708 <mazizollah@soldevelo.com>
2024-03-26 09:19:46 +0100

    added JS filter to dropdown, and changed templates

--- moving_motivators_py/settings.py
+++ moving_motivators_py/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'MM', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
